f57763450aa91581d8e1664a8dbf4083c9362435
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-22 20:09:45 +0100

    Register models on admin page

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from app.models import User
+from app.models import User, Tier, Bet, BetHouse, Game, GameOdd, Sport, Team
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Tier)
+admin.site.register(Bet)
+admin.site.register(BetHouse)
+admin.site.register(Game)
+admin.site.register(GameOdd)
+admin.site.register(Sport)
+admin.site.register(Team)
