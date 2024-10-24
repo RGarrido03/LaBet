3096380e069c2b74532e639ac31f595b773b7c69
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-24 16:01:40 +0100

    Add tier data migration

---
+++ resources/data_tier.sql
@@ -0,0 +1,3 @@
+INSERT INTO app_tier(name, charts_included, max_bets, max_wallet, min_arbitrage, price) VALUES ('Free', false, 5, 20, 0.95, 0);
+INSERT INTO app_tier(name, charts_included, max_bets, max_wallet, min_arbitrage, price) VALUES ('Pro', true, 15, 50, 0.90, 3);
+INSERT INTO app_tier(name, charts_included, max_bets, max_wallet, min_arbitrage, price) VALUES ('Ultra', true, 30, 200, 0.7, 5);
