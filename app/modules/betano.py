e0312c791886cd95480bc06168c2ef7bacedab41
André Ribeiro <andrepedro2004@hotmail.com>
2024-10-25 13:07:07 +0100

    betano doneee

--- app/modules/betano.py
+++ app/modules/betano.py
@@ -1,24 +1,51 @@
+import datetime
 import json
-from typing import Any, Optional
+from typing import Any, Optional, override
 import requests
 from app.models import *
 from app.modules.scrapper import Scrapper
+import http.client
+import gzip
+import io
 
 
 class BetanoScrapper(Scrapper):
     def __init__(self):
         super().__init__(
-            ...,
-            ...,
-            ...,
-            ...,
-            ...,
-            ...,
-            ...,
-            ...,
+            "Betano",
+            "betano.pt",
+            "https://www.betano.pt/assets/images/logo.svg",  # change
+            lambda event: event["markets"][0]["selections"][0]["fullName"],
+            lambda event: event["markets"][0]["selections"][2]["fullName"],
+            lambda event: datetime.datetime.fromtimestamp(
+                event["startTime"] / 1000
+            ).isoformat()
+            + "Z",  # vem em epoch
+            lambda event: event["markets"][0]["selections"][0]["price"],
+            lambda event: event["markets"][0]["selections"][1]["price"],
+            lambda event: event["markets"][0]["selections"][2]["price"],
         )
         self.bet_house = self.get_or_create_bet_house()
         self.url = "https://www.betano.pt/api/sport/futebol/jogos-de-hoje/"
+        self.base_url = "/api/sport/futebol/jogos-de-hoje/"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+            "Accept-Language": "pt-PT,pt;q=0.8,en;q=0.5,en-US;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Connection": "keep-alive",
+            "Cookie": "__cf_bm=0fWu3pO0Brpp1j8UyBloUXnzc5t0OfRZC3TfZLvcRFc-1729851634-1.0.1.1-FTq1wwyKFMm3.lH2cpR4LEHXMlMAe_PFhlBpBiqzVruD8riKDpCmOiPc3AwcvaeRcZYfKqgUZVaomZVFmHwHAQ; _cfuvid=lCpNIU0wlFZ6F3dIkG2oiLneTedFVc8ZabZV6lk8xmM-1729851634898-0.0.1.1-604800000",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Priority": "u=0, i",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "TE": "trailers",
+        }
+        self.conn = http.client.HTTPSConnection("www.betano.pt")
 
     def get_or_create_bet_house(self) -> BetHouse:
         """Retrieve or create the Betano BetHouse object."""
@@ -27,77 +54,65 @@ class BetanoScrapper(Scrapper):
             return BetHouse.objects.create(name="Betano", logo="", website="betano.pt")
         return db_obj
 
+    def _fetch_page(self, latest_id) -> list:
+        """Fetch a page of data from the API and return it as a list of games."""
+
+        params = f"?latestId={latest_id}&req=mb" if latest_id else ""
+        self.conn.request("GET", self.base_url + params, headers=self.headers)
+        response = self.conn.getresponse()
+
+        compressed_data = response.read()
+        if response.getheader("Content-Encoding") == "gzip":
+            with gzip.GzipFile(
+                fileobj=io.BytesIO(compressed_data)
+            ) as decompressed_file:
+                decompressed_data = decompressed_file.read()
+        else:
+            decompressed_data = compressed_data
+
+        decoded_data = decompressed_data.decode("utf-8")
+        return json.loads(decoded_data)
+
     def scrap(self) -> list[GameOdd]:
         """Fetch data from API and parse the response."""
-        try:
-            import http.client
-            import gzip
-            import io
+        all_games = []
+        latest_id = 0
+        now = datetime.datetime.now()
 
-            # Configurando a conexão e os headers
-            conn = http.client.HTTPSConnection("www.betano.pt")
-            headers = {
-                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-                "Accept-Language": "pt-PT,pt;q=0.8,en;q=0.5,en-US;q=0.3",
-                "Accept-Encoding": "gzip, deflate, br, zstd",
-                "Connection": "keep-alive",
-                "Cookie": "__cf_bm=0fWu3pO0Brpp1j8UyBloUXnzc5t0OfRZC3TfZLvcRFc-1729851634-1.0.1.1-FTq1wwyKFMm3.lH2cpR4LEHXMlMAe_PFhlBpBiqzVruD8riKDpCmOiPc3AwcvaeRcZYfKqgUZVaomZVFmHwHAQ; _cfuvid=lCpNIU0wlFZ6F3dIkG2oiLneTedFVc8ZabZV6lk8xmM-1729851634898-0.0.1.1-604800000",
-                "Upgrade-Insecure-Requests": "1",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1",
-                "Priority": "u=0, i",
-                "Pragma": "no-cache",
-                "Cache-Control": "no-cache",
-                "TE": "trailers",
-            }
+        time_in_24h = now + datetime.timedelta(hours=24)
 
-            # Fazendo a requisição GET
-            conn.request("GET", "/api/sport/futebol/jogos-de-hoje/", headers=headers)
-            response = conn.getresponse()
+        timestamp_in_24h = int(time_in_24h.timestamp())
+        last_game_time = 0
 
-            # Lendo os dados da resposta
-            compressed_data = response.read()
+        while (
+            timestamp_in_24h > last_game_time
+        ):  # adicionar condicao de paragem , tipo todos os jogos de hoje
+            try:
+                data = self._fetch_page(latest_id)
 
-            # Verificando se a resposta está compactada em gzip
-            if response.getheader("Content-Encoding") == "gzip":
-                # Descomprimindo os dados gzip
-                with gzip.GzipFile(
-                    fileobj=io.BytesIO(compressed_data)
-                ) as decompressed_file:
-                    decompressed_data = decompressed_file.read()
-            else:
-                decompressed_data = compressed_data
+                games = data["data"]["blocks"][0]["events"]
 
-            # Decodificando os dados
-            decoded_data = decompressed_data.decode("utf-8")
+                # start time
+                # data["data"]["blocks"][0]["events"][x]["startTime"]  # in epoch
 
-            # Exibindo o conteúdo
-            print(decoded_data)
+                parsed_games = [
+                    match
+                    for event in games
+                    if (match := self.parse_event(event))
+                    and event["markets"][0]["name"].lower() == "resultado final"
+                ]
 
-            self.data = response.json()  # Load the response as JSON
-            print(self.data)
-            # self.parsed_data = self.parse_json()
-        except requests.RequestException as e:
-            self.logger.error(f"Error fetching data: {e}")
-        except json.JSONDecodeError as e:
-            self.logger.error(f"Error decoding JSON: {e}")
+                if not parsed_games:
+                    break  # Stop if no more games are returned
+
+                all_games.extend(parsed_games)
+                latest_id = data["data"]["blocks"][0]["events"][-1]["id"]
+                last_game_time = data["data"]["blocks"][0]["events"][-1]["startTime"]
+            except Exception as e:
+                self.logger.error(f"Error fetching data: {e}")
+                break
+
+        # return all_games
+        self.parsed_data = all_games
 
         return self.parsed_data
-
-    def parse_event(self, event: dict[str, Any]) -> Optional[GameOdd]:
-        """Parse a single event and return a GameOdd object if valid."""
-        # Parse the event
-        return None
-
-    def get_team_from_event(self, event: dict[str, Any], index: int) -> Optional[Team]:
-        """Extract and return a Team object from the event data."""
-        # Extract team data
-        return None
-
-    def parse_json(self) -> list[GameOdd]:
-        """Parse the fetched JSON data and return the cleaned list of GameOdd."""
-        # Parse the JSON data
-        return []
