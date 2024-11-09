e0988dfb5f4b8d4e23c753ad69bf9f1192632311
André Ribeiro <andrepedro2004@hotmail.com>
2024-11-09 17:01:01 +0000

    changing comments

--- labet/scheduler.py
+++ labet/scheduler.py
@@ -6,7 +6,7 @@ def create_schedule(apps, schema_editor):
         Schedule.objects.create(
             func='labet.tasks.perform_scraping',
             schedule_type=Schedule.MINUTES,
-            minutes=10,  # Execute a cada 30 segundos
+            minutes=10,  # Execute a cada 10 MIN
             repeats=-1  # Repetições infinitas
         )
         # Add another schedule and then run migration
