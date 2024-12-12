09d3dd8dc47f189ced8db9d6212ea136dbaa78a3
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-12-12 17:32:40 +0000

    Fix password hashing on user create

--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from app.models import *
@@ -28,6 +29,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
 
     class Meta:
         model = User
