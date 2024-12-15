b6973abccf8020ce1b73ad81f056cd08f34e82d8
André Ribeiro <andrepedro2004@hotmail.com>
2024-12-15 02:15:07 +0000

    ruben, done, testado, sem copiloto, tudo manual

--- app/views/team.py
+++ app/views/team.py
@@ -35,6 +35,12 @@ def get_team(request: Request, team_id) -> Response:
 
 
 class TeamsView(generics.ListAPIView):
-    queryset = Team.objects.order_by("normalized_name").all()
     serializer_class = TeamSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Team.objects.order_by("normalized_name").all()
+        name = self.request.query_params.get("name", None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
